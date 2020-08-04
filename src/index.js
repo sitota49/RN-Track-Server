92774bdb9682bb005320bdb408dc5a23f992993e
sitota49 <se.sitota.ezra@gmail.com>
2020-08-04 14:42:09 +0300

    Express server init setup

---
+++ src/index.js
@@ -0,0 +1,34 @@
+require('./models/user');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+const mongoUri = 'mongodb+srv://admin:passwordpassword@cluster0.mkxey.mongodb.net/<dbname>?retryWrites=true&w=majority'
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongoose instance')
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error('Error connectiong ', err)
+});
+
+
+app.get('/', (req, res) => {
+    res.send('Hi there!');
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
