05baf14842bf9109a4a95ff5d9f1cc9f90413d90
sitota49 <se.sitota.ezra@gmail.com>
2020-08-07 12:28:00 +0300

    useLocation Hook

--- tracks/src/screens/trackCreateScreen.js
+++ tracks/src/screens/trackCreateScreen.js
@@ -1,42 +1,28 @@
 import '../_mockLocation';
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useContext } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements'
 import Map from '../components/Map';
-import { requestPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location';
-import { SafeAreaView } from 'react-navigation';
+import useLocation from '../hooks/useLocation'
+import { SafeAreaView, withNavigationFocus } from 'react-navigation';
 import { Context as LocationContext } from '../context/LocationContext';
-
-const TrackCreateScreen = () => {
-    const { addLocation } = useContext(LocationContext);
-    const [err, setErr] = useState(null);
-
-    const startWatching = async () => {
-        try {
-            await requestPermissionsAsync();
-            await watchPositionAsync({
-                accuracy: Accuracy.BestForNavigation,
-                timeInterval: 1000,
-                distanceInterval: 10
-            }, (location) => { addLocation(location) });
-        } catch (e) {
-            setErr(e);
-        }
-    };
-
-    useEffect(() => {
-        startWatching();
-    }, []);
+import TrackForm from '../components/TrackForm';
+const TrackCreateScreen = ({ isFocused }) => {
+    const { state, addLocation } = useContext(LocationContext);
+    const [err] = useLocation(isFocused, location => {
+        addLocation(location, state.recording);
+    });
 
     return (
         <SafeAreaView forceInset={{ top: "always" }}>
             <Text h2>Create a Track</Text>
             <Map />
             {err ? <Text>Please enable locations services</Text> : null}
+            <TrackForm />
         </SafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({});
 
-export default TrackCreateScreen;
+export default withNavigationFocus(TrackCreateScreen);
